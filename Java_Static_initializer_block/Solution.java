ede7fc700b1af2b277d6429468798b7b6fde8a3e
Anuj <anujsinha789@gmail.com>
2022-01-20 12:08:23 +0530

    Solution Fix: Java Static Initializer Block

--- Java_Static_initializer_block/Solution.java
+++ Java_Static_initializer_block/Solution.java
@@ -6,20 +6,21 @@ import java.util.regex.*;
 
 public class Solution {
 
-static int B = 0 , H = 0;
+static Scanner input = new Scanner(System.in);
 static boolean flag = true;
-static {
-    Scanner in = new Scanner(System.in);
-    try {
-        B = in.nextInt();
-        H = in.nextInt();
-        if(B < 0 || H < 0){
+static int B = input.nextInt();
+static int H = input.nextInt();
+
+static{
+    try{
+        if(B <= 0 || H <= 0){
             flag = false;
-            throw new Exception();
+            throw new Exception("Breadth and height must be positive");
         }
-    } catch(Exception e){
-        System.out.println("java.lang.Exception: Breadth and height must be positive");
+    }catch(Exception e){
+        System.out.println(e);
     }
+
 }
 public static void main(String[] args){
 		if(flag){
