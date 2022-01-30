a07fa2353fdd53e0fc507c4000d61d4817c11bef
Anuj <anujsinha789@gmail.com>
2022-01-30 17:47:01 +0530

    Solution : Java MD5

---
+++ Java_MD5/Solution.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.security.MessageDigest;
+
+public class Solution {
+
+   public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        sc.close();
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(str.getBytes());
+            byte[] digest = md.digest();
+            for (byte b : digest) {
+                System.out.printf("%02x", b);
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
